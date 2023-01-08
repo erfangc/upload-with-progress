16ed8036d9f646ce324a35c2910f7fa6135d9913
Erfang Chen <erfangc@gmail.com>
2023-01-07 23:30:26 -0500

    initial commit

--- src/FileUploader/FileUploadDragAndDrop.js
+++ src/FileUploader/FileUploadDragAndDrop.js
@@ -14,14 +14,15 @@ export function FileUploadDragAndDrop() {
         <div className="space-y-4">
             <div
                 {...getRootProps()}
-                className={`rounded w-96 h-40 border-2 border-dashed items-center justify-center flex p-4 ${isDragActive ? 'border-green-500' : 'border-gray-400'}`}
+                className={`rounded duration-300 transition-all w-96 h-40 border-2 border-dashed items-center justify-center flex p-4 ${isDragActive ? 'border-emerald-500' : 'border-gray-400'}`}
             >
                 <input {...getInputProps()}/>
-                {
-                    isDragActive
-                        ? <p className="text-green-500">Let go to drop ...</p>
-                        : <p className="text-gray-600">Drag & drop files here, or click to select files</p>
-                }
+                <p className={`transition-all ${isDragActive ? 'text-emerald-500' : 'text-gray-500'}`}>
+                    {isDragActive
+                        ? 'Let go to drop ...'
+                        : 'Drag & drop files here, or click to select files'
+                    }
+                </p>
             </div>
             <StagedFileList/>
             <button onClick={handleSubmit} className="px-2 py-1.5 bg-blue-500 rounded text-white">
