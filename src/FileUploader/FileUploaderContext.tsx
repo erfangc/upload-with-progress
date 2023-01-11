785edd742af5f43dbd2e402c4b57db2ccc1f68b1
Erfang Chen <erfangc@gmail.com>
2023-01-10 19:16:16 -0500

    initial commit

--- src/FileUploader/FileUploaderContext.tsx
+++ src/FileUploader/FileUploaderContext.tsx
@@ -4,10 +4,10 @@ import {DropzoneState} from "react-dropzone";
 type FileUploaderContextType = {
     progress: number;
     handleSubmit: () => Promise<void>;
-    uploadingFile?: File,
-    busy: boolean,
-    stagedFiles: File[],
-    completedFiles: File[],
+    uploadingFile?: File;
+    busy: boolean;
+    stagedFiles: File[];
+    completedFiles: File[];
     dropzoneState: DropzoneState;
 };
 // @ts-ignore
